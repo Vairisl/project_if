f448dbe4087ece06f2401e2be1f8c6f628da79b4
Vairis <vairiks123@gmail.com>
2016-10-04 20:12:00 +0300

    Clean

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,8 @@
 <template>
-    <input type="text" v-model="username"> <br>
-    <input type="text"> <br>
-    <input type="submit" @click="submit()">
-    <div v-if="!username.length">
-        You must provide data!!
-    </div>
-
 </template>
 
 <script>
-export default{
-    data () {
-        return {
-            username: ""
-        }
-    },
-    methods: {
-        submit () {
-            if(this.username == "suka") {
-                alert("Fuck off")
-            }
-        }
-    }
-}
+export default {}
 </script>
 
 <style>
