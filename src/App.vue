d7e4092ae329be4abf9267be497802fb88fb8951
Vairis <vairiks123@gmail.com>
2016-10-04 20:59:50 +0300

    Dinamisks menu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
+	<ul>
+		<li v-for="item in menu"
+			v-bind:class="{ active: item.active}">
+			{{item.title}}
+		</li>
+	</ul>
 </template>
 
 <script>
-export default {}
+export default {
+	data () {
+		return {
+			menu: [
+				{
+					title: "Home",
+					active: true,
+				},
+				{
+					title: "Contacts",
+					active: false,
+				},
+			]
+		}
+	}
+}
 </script>
 
 <style>
+.active {
+	color: red;
+}
 </style>
 
