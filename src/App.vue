a3734c1eddad8e699d2706c45b3f3e460cfea858
Vairis <vairiks123@gmail.com>
2016-10-06 21:46:27 +0300

    New if

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,44 @@
 <template>
+<input v-model="car.horsePower"/>
+<input v-model="car.price"/>
+<br/>
+
+Plate number <b>{{car.plateNumber}}</b>
+</br>
+Horse power <b>{{car.horsePower}}</b>
+</br>
+Car price <b>{{car.isCheapCar}}</b>
+</br>
+
+<span v-if="isCoolCar(car)">Ahujenna</span>
+
+<span v-if="isCheapCar(car)">Cheap as shit</span>
+
 </template>
 
 <script>
-export default {}
+
+export default {
+	data () {
+		return{
+			car: {
+				plateNumber: "AA-3333",
+				horsePower: 1200,
+				price: 9999,
+			}
+		}
+	},
+	
+	methods: {
+		isCoolCar(car){
+			return (car.horsePower > 1000) 
+		},
+		isCheapCar(car){
+			return (car.price < 200)
+		}
+	}
+}
+
 </script>
 
 <style>
