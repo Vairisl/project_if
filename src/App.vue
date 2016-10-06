2ad9e22ad7ed31f511c3b383079d0cc1af27f631
Vairis <vairiks123@gmail.com>
2016-10-06 20:37:09 +0300

    New If

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,10 @@
 <template>
-	<ul>
-		<li v-for="item in menu"
-			v-bind:class="{ active: item.active}"
-			@click="goToPage(item)">
-			{{item.title}}
-		</li>
-	</ul>
 </template>
 
 <script>
-export default {
-	data () {
-		return {
-			menu: [
-				{
-					title: "Home",
-					active: true,
-				},
-				{
-					title: "Contacts",
-					active: false,
-				},
-			]
-		}
-	},
-	methods: {
-		goToPage(menuItem) {
-			for (let i in this.menu) {
-				this.menu[i].active = false
-			}
-			
-			menuItem.active = true
-		}
-	}
-}
+export default {}
 </script>
 
 <style>
-.active {
-	color: red;
-}
-
-li:hover {
-	color: blue;
-}
 </style>
 
